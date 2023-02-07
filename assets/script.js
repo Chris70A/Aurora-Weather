e1026de06ce2e84e9b847baa6011906e52cac395
Christian Plasencia <113798773+Chris70A@users.noreply.github.com>
2023-02-07 12:04:09 -0800

    finished basic HTML

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+var weatherApi = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"; //api url
+var searchBar = document.querySelector('#search');
+var todaysWeather = document.querySelector('#today');
+var futureWeather = document.querySelector('#future');
+var searchHistory = document.querySelector('#history');
+
+
+function DisplayWeather () {
+    
+}
+
+
+
